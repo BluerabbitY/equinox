1ac2bc9c73c7ad7f04b445866eb057265864edb8
BluerabbitY <BlueRabbitY@protonmail.com>
2025-10-08 17:00:40 +0800

    feat: init project
    * init project
    
    Signed-off-by: BluerabbitY <BlueRabbitY@protonmail.com>

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,4 +1,32 @@
-int main()
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQuickWindow>
+
+#ifdef BUILD_HUSKARUI_STATIC_LIBRARY
+#include <QtQml/qqmlextensionplugin.h>
+Q_IMPORT_QML_PLUGIN(HuskarUI_BasicPlugin)
+#endif
+
+#include <husapp.h>
+
+int main(int argc, char *argv[])
 {
-    return 0;
+    QQuickWindow::setDefaultAlphaBuffer(true);
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    HusApp::initialize(&engine);
+
+    engine.addImportPath(QCoreApplication::applicationDirPath() + "/qml");
+
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+    engine.loadFromModule("equinox", "main");
+
+    return app.exec();
 }
---
+++ src/qml/main.qml
@@ -0,0 +1,20 @@
+import QtQuick
+import HuskarUI.Basic
+
+HusWindow {
+    width: 640
+    height: 480
+    visible: true
+    title: qsTr('equinox')
+    captionBar.winIconVisible: false
+    Component.onCompleted: {
+        if (Qt.platform.os === 'windows') {
+            if (setSpecialEffect(HusWindow.Win_MicaAlt)) return;
+            if (setSpecialEffect(HusWindow.Win_Mica)) return;
+            if (setSpecialEffect(HusWindow.Win_AcrylicMaterial)) return;
+            if (setSpecialEffect(HusWindow.Win_DwmBlur)) return;
+        } else if (Qt.platform.os === 'osx') {
+            if (setSpecialEffect(HusWindow.Mac_BlurEffect)) return;
+        }
+    }
+}
